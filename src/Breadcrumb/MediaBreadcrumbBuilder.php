3df76d21672bebf03d7817aa0be7a2bd503efd32
Roberto Araya <roberto.araya@btester.cl>
2018-02-04 11:17:01 -0300

    by roberto_araya: Update MediaBreadcrumbBuilder class of bt_media.breadcrumb service.

--- src/Breadcrumb/MediaBreadcrumbBuilder.php
+++ src/Breadcrumb/MediaBreadcrumbBuilder.php
@@ -25,12 +25,13 @@ class MediaBreadcrumbBuilder implements BreadcrumbBuilderInterface {
    *
    * @var array
    */
-  private $routes = array(
+  private $routes = [
+    'entity.media.add_form',
     'entity.media.add_page',
     'entity.media.edit_form',
     'page_manager.page_view_app_website_media_app_website_media-panels_variant-0',
     'page_manager.page_view_app_website_media_app_website_media-panels_variant-1',
-  );
+  ];
 
   /**
    * {@inheritdoc}
@@ -42,9 +43,25 @@ class MediaBreadcrumbBuilder implements BreadcrumbBuilderInterface {
   /**
    * {@inheritdoc}
    */
-  public function applies(RouteMatchInterface $attributes) {
+  public function applies(RouteMatchInterface $routeMatch) {
     $match = $this->routes;
-    if (in_array($attributes->getRouteName(), $match)) {
+    if (in_array($routeMatch->getRouteName(), $match)) {
+      if ($routeMatch->getRouteName() == 'entity.media.add_form') {
+        if ($routeMatch->getParameters()->get('media_type')->get('id') != 'bt_documents') {
+          return TRUE;
+        }
+        else {
+          return FALSE;
+        }
+      }
+      elseif ($routeMatch->getRouteName() == 'entity.media.edit_form') {
+        if ($routeMatch->getParameters()->get('media')->bundle() != 'bt_documents') {
+          return TRUE;
+        }
+        else {
+          return FALSE;
+        }
+      }
       return TRUE;
     }
     else {
@@ -55,29 +72,23 @@ class MediaBreadcrumbBuilder implements BreadcrumbBuilderInterface {
   /**
    * {@inheritdoc}
    */
-  public function build(RouteMatchInterface $route_match) {
-    $route = $route_match->getRouteName();
+  public function build(RouteMatchInterface $routeMatch) {
+    $route = $routeMatch->getRouteName();
     $breadcrumb = new Breadcrumb();
     $breadcrumb->addCacheContexts(["url"]);
 
     $breadcrumb->addLink(Link::createFromRoute($this->siteName, 'page_manager.page_view_app_app-panels_variant-0'));
     $breadcrumb->addLink(Link::createFromRoute('Website', 'page_manager.page_view_app_website_app_website-panels_variant-0'));
 
-    switch ($route) {
-      case 'entity.media.add_page':
-        $breadcrumb->addLink(Link::createFromRoute('Multimedia', 'page_manager.page_view_app_website_media_app_website_media-panels_variant-0'));
-        break;
-
-      case 'entity.media.edit_form':
-        $mid = $route_match->getParameters()->get('media')->bundle();
-        if ($mid == 'bt_documents') {
-          $breadcrumb->addLink(Link::createFromRoute('Documents', 'page_manager.page_view_app_website_documents_app_website_documents-panels_variant-0'));
-        }
-        else {
-          $breadcrumb->addLink(Link::createFromRoute('Multimedia', 'page_manager.page_view_app_website_media_app_website_media-panels_variant-0'));
-        }
-        break;
-
+    if (in_array($route, [
+      'entity.media.add_page',
+      'entity.media.edit_form',
+      'entity.media.add_form',
+    ])) {
+      $breadcrumb->addLink(Link::createFromRoute('Media', 'page_manager.page_view_app_website_media_app_website_media-panels_variant-0'));
+      if ($route == 'entity.media.add_form') {
+        $breadcrumb->addLink(Link::createFromRoute('Add media', 'entity.media.add_page'));
+      }
     }
 
     return $breadcrumb;
