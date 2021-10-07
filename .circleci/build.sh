3b7323280c225e1d432ee03d36a34820f3f030cd
Roberto Araya <roberto.araya@btester.cl>
2021-10-07 14:06:30 -0300

    Change .circleci/build.sh to avoid memory allocation error in circleci.

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -117,6 +117,8 @@ mkdir -p "${BUILD_DIR}/web/modules/${MODULE}"
 ln -s "$(pwd)"/* "${BUILD_DIR}/web/modules/${MODULE}" && rm "${BUILD_DIR}/web/modules/${MODULE}/${BUILD_DIR}"
 
 echo "==> Enable module ${MODULE}."
+"${BUILD_DIR}/vendor/bin/drush" -r build/web pm:enable bt_core -y
+"${BUILD_DIR}/vendor/bin/drush" -r build/web pm:enable bt_cms -y
 "${BUILD_DIR}/vendor/bin/drush" -r "${BUILD_DIR}/web" pm:enable "${MODULE}" -y
 "${BUILD_DIR}/vendor/bin/drush" -r "${BUILD_DIR}/web" cr
 
